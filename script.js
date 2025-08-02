0aba4b8e8ce9da71a9b69ca601c8692ee5ce0bb4
Ayga360 <williamgbadebooke@gmail.com>
2025-08-02 02:06:46 +0100

    Added theme toggle

--- script.js
+++ script.js
@@ -2,3 +2,22 @@
 window.onload = function () {
   alert("Welcome to Liam's Portfolio!");
 };
+
+const toggle = document.getElementById("theme-toggle");
+const body = document.body;
+
+// Check local storage
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark-mode");
+  toggle.checked = true;
+}
+
+toggle.addEventListener("change", () => {
+  if (toggle.checked) {
+    body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark");
+  } else {
+    body.classList.remove("dark-mode");
+    localStorage.setItem("theme", "light");
+  }
+});
